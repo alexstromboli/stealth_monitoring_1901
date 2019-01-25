2f6cfa2aec2230ff8f349cf55bd489f2066fe5cf
Alexey Piskarev <stromboli@yandex.ru>
2019-01-25 21:05:15 +0300

    Parsing command line parameters (date)

--- GetchMarsRoverPhoto/Program.cs
+++ GetchMarsRoverPhoto/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace GetchMarsRoverPhoto
 {
@@ -6,7 +10,34 @@ namespace GetchMarsRoverPhoto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine ("Usage:");
+                Console.WriteLine (Path.GetFileName (Environment.GetCommandLineArgs()[0])
+                    + " <api-key> <date>");
+
+                return;
+            }
+
+            // parameters
+            string ApiKey = args[0];
+            string strDate = args[1];
+
+            // parse date
+            DateTime dtDay;
+            CultureInfo ciEnglish = new CultureInfo ("en-us");
+            if (new[]
+                {
+                    "MM/dd/yy",
+                    "MMMM d, yyyy",
+                    "MMM-d-yyyy"
+                }
+                .All (f => !DateTime.TryParseExact (strDate, f, ciEnglish, DateTimeStyles.None, out dtDay))
+                )
+            {
+                Console.WriteLine ("Wrong date format: " + strDate);
+                return;
+            }
         }
     }
 }
