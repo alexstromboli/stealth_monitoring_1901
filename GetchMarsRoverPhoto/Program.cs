d847738541ff25e19b2715689e6a4b1d0112d94f
Alexey Piskarev <stromboli@yandex.ru>
2019-01-25 20:22:28 +0300

    Template-based console app

---
+++ GetchMarsRoverPhoto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetchMarsRoverPhoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
